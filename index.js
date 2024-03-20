91c3d98b4ed43d0b2887343f4e0b8f707692fd24
c-nunez <nunez_cr@outlook.com>
2024-03-20 00:03:04 -0400

    undo OOP code

--- index.js
+++ index.js
@@ -1,38 +1,29 @@
 import express from "express";
 import { exec } from "child_process";
-class CPUMonitor {
-    constructor() {
-        this.app = express();
-        this.port = 3000;
-        this.startRoutes();
-    }
 
-    startRoutes() {
-        this.app.use(express.static("public"));
+const app = express();
+const port = 3000;
 
-        this.app.get("/", (req, res) => {
-            res.render("index.ejs");
-        });
+app.use(express.static("public"));
 
-        this.app.get('/cpu', this.handleCPURoute.bind(this));
+app.get("/", (req, res) => {
+        res.render("index.ejs");
+});
 
-        this.app.listen(this.port, () => {
-            console.log(`Server running on port: ${this.port}`);
-        });
-    }
+app.get('/cpu', (req, res) => {
+    exec('mpstat 1 1', (err, stdout, stderr) => {
+        if (err) {
+        console.error(err);
+        return res.status(500).send('Error retrieving CPU usage');
+        }    
+    const lines = stdout.split('\n');    
+    lines.forEach(line => {
+    res.write(line + '\n');
+    });    
+    res.end();
+});
+});
 
-    handleCPURoute(req, res) {
-        exec('mpstat 1 1', (err, stdout, stderr) => {
-            if (err) {
-                console.error(err);
-                return res.status(500).send('Error retrieving CPU usage');
-            }    
-            const lines = stdout.split('\n');    
-            lines.forEach(line => {
-                res.write(line + '\n');
-            });    
-            res.end();
-        });
-    }
-}
-const cpuMonitor = new CPUMonitor();
+app.listen(port, ()=> {
+    console.log(`Server running on port: ${port}`);
+});
