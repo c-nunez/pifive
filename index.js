1553d61e0a6a2d0abfa89256d6f02dd1da2740bd
c-nunez <nunez_cr@outlook.com>
2024-03-19 23:59:10 -0400

    Front page: added cpu stats tester

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 import express from "express";
+import { exec } from "child_process";
+class CPUMonitor {
+    constructor() {
+        this.app = express();
+        this.port = 3000;
+        this.startRoutes();
+    }
 
-const app = express();
-const port = 3000;
+    startRoutes() {
+        this.app.use(express.static("public"));
 
-app.use(express.static("public"));
+        this.app.get("/", (req, res) => {
+            res.render("index.ejs");
+        });
 
-app.get("/", (req, res) => {
+        this.app.get('/cpu', this.handleCPURoute.bind(this));
 
-    res.render("index.ejs");
-});
+        this.app.listen(this.port, () => {
+            console.log(`Server running on port: ${this.port}`);
+        });
+    }
 
-
-app.listen(port, ()=> {
-    console.log(`Server running on port: ${port}`);
-});
+    handleCPURoute(req, res) {
+        exec('mpstat 1 1', (err, stdout, stderr) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).send('Error retrieving CPU usage');
+            }    
+            const lines = stdout.split('\n');    
+            lines.forEach(line => {
+                res.write(line + '\n');
+            });    
+            res.end();
+        });
+    }
+}
+const cpuMonitor = new CPUMonitor();
